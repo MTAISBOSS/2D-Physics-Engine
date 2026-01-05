60c444c6a8a26fee64faa40eb6679424796d9bd6
mtaisboss <mohammad.1387.kavian@gmail.com>
2026-01-05 13:26:02 +0330

    Implemented vector and mathematics structs

---
+++ Physics-Engine/Physics-Engine/Engine.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Physics_Engine
+{
+    public class Engine
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello Physics");
+        }
+    }
+}
